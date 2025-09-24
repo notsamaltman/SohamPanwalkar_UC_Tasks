bf1683e33ce275f16106ed4f5309dcd169fc395b
notsamaltman <panwalkarsoham@gmail.com>
2025-09-24 13:48:27 +0530

    added customuser model to page and customised login and register html pages created register user views

--- task-3/diary_project/diary/models.py
+++ task-3/diary_project/diary/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    ]
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True, blank=True)
+    
+    email = models.EmailField(unique=True)  
+
+    def __str__(self):
+        return self.username
--- task-3/diary_project/diary/views.py
+++ task-3/diary_project/diary/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CustomUser
+from django.contrib import messages
 
 def login(request):
     return render(request, 'diary/diary_template.html')
+
+def register(request):
+    return render(request, 'diary/diary_register.html')
+
+def register_user(request):
+    if request.method=="POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        age = request.POST['age']
+        gender = request.POST['gender']
+        password = request.POST['password1']
+
+        # Check if username or email already exists
+        if CustomUser.objects.filter(username=username).exists():
+            messages.error(request, "Username is already taken.")
+            return redirect('register')
+
+        if CustomUser.objects.filter(email=email).exists():
+            messages.error(request, "Email is already registered.")
+            return redirect('register')
+
+        # Create user
+        user = CustomUser.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            age=age,
+            gender=gender
+        )
+        messages.success(request, "Account created successfully! Please login.")
+        return redirect('login')
+    return render(request, 'register')
